22d1d6d928718dc50da578eec336db7966b21b4f
jiwen xin <kevinxin@scripps.edu>
2019-08-29 08:28:03 -0700

    initial commit

---
+++ parser.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def load_data(data_folder=None):
+    url = 'http://mygene.info/v3/query?q=_exists_:go.BP&fields=go.BP&fetch_all=TRUE'
+    cnt = 0
+    total = 1
+    pathway_ids = set()
+    while cnt < total:
+        doc = requests.get(url).json()
+        total = doc['total']
+        cnt += len(doc['hits'])
+        url = 'http://mygene.info/v3/query?scroll_id=' + doc['_scroll_id']
+        for _doc in doc['hits']:
+            info = _doc['go']['BP']
+            if isinstance(info, dict):
+                info = [info]
+            for record in info:
+                _id = record['id']
+                if _id not in pathway_ids:
+                    pathway_ids.add(_id)
+                    yield {'_id': _id,
+                           'name': record['term']}
